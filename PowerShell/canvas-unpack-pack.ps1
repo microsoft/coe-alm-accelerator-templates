36beb09bd996ecb4d3e35cbca752a03bd91a2d63
Mike Ochs <42348035+mikefactorial@users.noreply.github.com>
2022-04-29 10:13:38 -0400

    Reverting to using PAC CLI for canvas unpack / pack

--- PowerShell/canvas-unpack-pack.ps1
+++ PowerShell/canvas-unpack-pack.ps1
@@ -1,47 +1,12 @@
 ﻿function Invoke-CanvasUnpackPack($packOrUnpack, $source, $destination) {
-    Write-Host "Loading Assemblies"
-    Get-ChildItem -Path "..\PowerAppsLanguageTooling\" -Recurse -Filter *.dll | 
-    ForEach-Object {
-        [System.Reflection.Assembly]::LoadFrom($_.FullName)
-    }
-    
     if ($packOrUnpack -eq 'pack') {
         Write-Host "Packing $source to $destination"
-        $results = [Microsoft.PowerPlatform.Formulas.Tools.CanvasDocument]::LoadFromSources($source)
-        if($results.HasErrors) {
-            throw $results.Item2.ToString();
-            return
-        } else {
-            Write-Host $results.Item2.ToString()
-        }
-        $saveResults = $results.Item1.SaveToMsApp($destination)
-        if($saveResults.HasErrors) {
-            throw $saveResults.ToString();
-            return
-        } 
-        else {
-            Write-Host $saveResults.ToString()
-        }
+        ..\PowerPlatformCLI\pac.exe canvas pack --sources $source --msapp $destination
     }
     else {
         if ($packOrUnpack -eq 'unpack') {
             Write-Host "Unpacking $source to $destination"
-            $results = [Microsoft.PowerPlatform.Formulas.Tools.CanvasDocument]::LoadFromMsapp($source)
-            if($results.HasErrors) {
-                throw $results.Item2.ToString();
-                return
-            } else {
-                Write-Host $results.Item2.ToString()
-            }
-    
-            $saveResults = $results.Item1.SaveToSources($destination)
-            if($saveResults.HasErrors) {
-                throw $saveResults.ToString();
-                return
-            } 
-            else {
-                Write-Host $saveResults.ToString()
-            }
+            ..\PowerPlatformCLI\pac.exe canvas unpack --msapp $source --sources $destination
         }
         else {
             throw "Invalid packOrUnpack parameter. Must be 'pack' or 'unpack'.";
