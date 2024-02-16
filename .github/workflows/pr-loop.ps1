3578522b0b08ed3dad163470003bdd4ab17a5cd5
Rajeev Pentyala <rajeevpe@microsoft.com>
2024-02-16 15:32:42 +0530

    PR Loop Changes

--- .github/workflows/pr-loop.ps1
+++ .github/workflows/pr-loop.ps1
@@ -1,5 +1,4 @@
-function Invoke-EndToEndPipelineTest ($User, $Password, $Data) {
-    $result = az login --allow-no-subscriptions -u $User -p $Password
+function Invoke-EndToEndPipelineTest ($Data) {
     $path = './PowerShell/tests/e2e-pipeline.tests.ps1'    
     $container = New-PesterContainer -Path $path -Data $Data
     Invoke-Pester -Container $container
